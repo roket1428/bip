80291e0109778e5ff3b569ba6f91df036588b926
roket1428 <meorhan@protonmail.com>
2023-12-26 18:49:35 +0300

    Initial frontend design and components
    Created FileUpload component for uploading files to our endpoint (incomplete)
    Created initial frontend desing using bootstrap and custom styles (also incomplete)

--- bip_frontend/src/App.js
+++ bip_frontend/src/App.js
@@ -1,10 +1,45 @@
-import './App.css';
+import './styles/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-reboot.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+import { Container, Stack } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import FileUpload from './components/FileUpload';
 
 function App() {
   return (
-    <h1>
-      BIP BIP
-    </h1>
+    <Stack>
+      <Navbar className="bg-navbar rounded-4 rounded-bottom-0 m-1 mb-0 shadow-sm">
+        <Container className='justify-content-center'>
+          <Navbar.Brand>
+            <img
+              alt=""
+              src="/img/favicon-32x32.png"
+              width="32"
+              height="32"
+              className="d-inline-block align-top"
+            />{' '}
+            Bilgi İşleme Projesi
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Navbar className="bg-navbar rounded-4 rounded-top-0 m-1 mt-0 shadow-sm">
+        <Container fluid>
+          <Button size='lg' className='btn-menu rounded-pill'>
+            <i class="bi bi-caret-left-fill"></i>
+          </Button>
+          <h1 className='text-center'>2022-2023 Yılı Mezun Adayları Listesi</h1>
+          <Button size='lg' className='btn-menu rounded-pill'>
+            <i class="bi bi-caret-right-fill"></i>
+          </Button>
+        </Container>
+      </Navbar>
+      <Container className='d-flex fileupload-container'>
+        <FileUpload />
+      </Container>
+    </Stack>
   );
 }
 
---
+++ bip_frontend/src/components/FileUpload.js
@@ -0,0 +1,20 @@
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+// TODO: implement sending files to the api endpoint using axios.
+function FileUpload() {
+    const onDrop = useCallback(acceptedFiles => {
+        console.log("test");
+    }, []);
+    const { getRootProps, getInputProps, IsDragActive } = useDropzone({ onDrop })
+
+    return (
+        <div className="fileupload-area rounded-4 shadow-sm" {...getRootProps()}>
+            <input {...getInputProps()} />
+            <i class="bi bi-file-earmark-arrow-up-fill fs-1"></i>
+            <p className="m-0">{IsDragActive ? 'Buraya sürükle.' : 'Dosya yükleyin.'}</p>
+        </div>
+    );
+}
+
+export default FileUpload;
